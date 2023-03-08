4583ff0b0d5403e48649596e2a39bfd2e5d49acd
TalhaMV <vaincetalha@gmail.com>
2023-03-07 21:23:08 -0500

    adding popups

--- script.js
+++ script.js
@@ -6,24 +6,104 @@ const map = new mapboxgl.Map({
 
     style: 'mapbox://styles/talhav/cle0a9fvt004d01qwzhe941ll',
 
-    center: [-79.39830123888215, 43.664695891060376], 
+    center: [-79.39830123888215, 43.664695891060376],
 
     zoom: 12,
 
 });
 
-map.addSource('streetcarstops', {
-    type: 'vector',
-    url: 'mapbox://talhav.6g3w7l9x'
-});
 
-map.map.addLayer({
-    'id': 'stops',
-    'type': 'circle',
-    'source': 'streetcarstops',
-    'source-layer': 'streetcarstops-bfbajq',
-    'paint': {
-    'circle-radius': 10,
-    'circle-color': 'red'
-}
-})
+map.on('load', () => {
+
+
+
+    //Add datasource from GeoJSON
+
+    map.addSource('streetcar', {
+
+        type: 'geojson',
+
+        data: 'https://raw.githubusercontent.com/TalhaMV/Lab3/main/streetcarstops.geojson'
+
+        //'https://smith-lg.github.io/ggr472-wk6-demo/data/torontomusicvenues.geojson'
+
+    });
+
+
+
+    map.addLayer({
+
+        'id': 'scar',
+
+        'type': 'circle',
+
+        'source': 'streetcar',
+
+        'paint': {
+
+            'circle-radius': 3,
+
+            'circle-color': 'red'
+
+        }
+
+    });
+
+    map.addSource('college', {
+
+        'type': 'vector',
+
+        'url': 'mapbox://talhav.0d4na3al'
+
+    });
+
+
+
+
+    map.addLayer({
+
+        'id': 'provterr-fill',
+
+        'type': 'fill',
+
+        'source': 'college',
+
+        'paint': {
+
+            'fill-color': '#000000',
+
+            'fill-opacity': 0.5,
+
+            'fill-outline-color': 'purple'
+
+        },
+
+        'source-layer': 'uoftcolleges-5imo5h'
+
+    });
+
+
+    map.on('click', 'scar', (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.name;
+        const network = e.features[0].properties.netowrk;
+        const position = e.features[0].properties.stop_position;
+
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML("<h1> " + description + "</h1>" + "<br>" + decription + "<br>" + network + "<br>" + position)
+            .addTo(map);
+    });
+
+
+
+});
