dc18f7af62e0ee213d5dfbb1a94d8a7252d882e6
TalhaMV <vaincetalha@gmail.com>
2023-03-07 23:02:26 -0500

    Popup Descriptions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
 
     container: 'map',
 
-    style: 'mapbox://styles/talhav/cle0a9fvt004d01qwzhe941ll',
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
 
     center: [-79.39830123888215, 43.664695891060376],
 
@@ -82,13 +82,25 @@ map.on('load', () => {
 
     });
 
+    // Add a layer showing the places.
+map.addLayer({
+    'id': 'labels',
+    'type': 'symbol',
+    'source': 'streetcar',
+    'layout': {
+    'icon-image': ['get', 'icon'],
+    'icon-allow-overlap': true
+    }
+    });
+     
+
 
     map.on('click', 'scar', (e) => {
         // Copy coordinates array.
         const coordinates = e.features[0].geometry.coordinates.slice();
         const description = e.features[0].properties.name;
-        const network = e.features[0].properties.netowrk;
-        const position = e.features[0].properties.stop_position;
+        const network = e.features[0].properties.network;
+        const shelter = e.features[0].properties.shelter;
 
 
         // Ensure that if the map is zoomed out such that multiple
@@ -100,8 +112,18 @@ map.on('load', () => {
 
         new mapboxgl.Popup()
             .setLngLat(coordinates)
-            .setHTML("<h1> " + description + "</h1>" + "<br>" + decription + "<br>" + network + "<br>" + position)
+            .setHTML("<p2> " + "name:"+ description + "</p2>" +  "<br>" + "<p2>" + "network:" + network + "</p2>" +"<br>" +"<p2>" + "shelter:" + shelter + "</p2>")
             .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'scar', () => {
+    map.getCanvas().style.cursor = 'pointer';
+    });
+     
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'scar', () => {
+    map.getCanvas().style.cursor = '';
     });
 
 
